fb4a60c77ce2f721e61a568e1a960b6ea872f17d
binoll <onionlsfa@gmail.com>
2025-03-06 02:46:38 +0300

    fix: fixed an error, the logger was not created

--- logger/logger.hpp
+++ logger/logger.hpp
@@ -1,28 +1,24 @@
 #pragma once
-
 #include <spdlog/spdlog.h>
 #include <memory>
 
 /**
- * @brief Глобальный логгер для всего проекта.
- * @details Инициализируется при первом использовании.
+ * @brief Глобальный логгер для всего проекта
+ * @details Обеспечивает:
+ * - Единую точку инициализации
+ * - Потокобезопасность
+ * - Ротацию логов
  */
 class GlobalLogger {
-public:
+ public:
   /// @brief Получить экземпляр логгера
-  static std::shared_ptr<spdlog::logger> get() {
-    static auto logger = create_logger();
-    return logger;
-  }
+  static std::shared_ptr<spdlog::logger> get();
 
-private:
-  /// @brief Создание логгера
-  static std::shared_ptr<spdlog::logger> create_logger() {
-    auto logger = spdlog::basic_logger_mt("global", "logs/global.log");
-    logger->set_level(spdlog::level::debug); // Уровень логирования
-    logger->set_pattern("[%Y-%m-%d %T.%e] [%l] %v"); // Формат сообщений
-    return logger;
-  }
+ private:
+  /// @brief Инициализация логгера
+  static void initialize();
+
+  static std::shared_ptr<spdlog::logger> logger_;
 };
 
 /// @brief Макрос для удобного доступа к логгеру
